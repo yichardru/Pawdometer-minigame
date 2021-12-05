6f40186b121221549516d1c110e4a6570a3dcb88
thicczucc <56744122+thicczucc@users.noreply.github.com>
2021-12-04 21:05:18 -0800

    update leaderboard

--- Assets/Scripts/FirebaseScripts/FirebaseLeaderboard.cs
+++ Assets/Scripts/FirebaseScripts/FirebaseLeaderboard.cs
@@ -1,27 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_WEBGL
+#if (UNITY_WEBGL && !UNITY_EDITOR)
+using FirebaseWebGL.Scripts.FirebaseBridge;
+using FirebaseWebGL.Scripts.Objects;
 #else
 using Firebase.Database;
 using Firebase.Auth;
 #endif
 using TMPro;
+using SimpleJSON;
 
 public class FirebaseLeaderboard : MonoBehaviour
 {
-    #if UNITY_WEBGL
-    #else
-    private DatabaseReference dbr;
     private static bool isCurrentlyReading = false;
     public GameObject scorePrefab;
     public Transform content;
+    #if (UNITY_WEBGL && !UNITY_EDITOR)
+    #else
+    private DatabaseReference dbr;
 
     async void Start()
     {
         dbr = FirebaseDatabase.DefaultInstance.RootReference;
     }
-
+    #endif
     public void RunDisplayScore()
     {
         var children = new List<GameObject>();
@@ -29,9 +32,13 @@ public class FirebaseLeaderboard : MonoBehaviour
         children.ForEach(child => Destroy(child));
         StartCoroutine(DisplayScores());
     }
-
+    
     public IEnumerator DisplayScores()
     {
+#if (UNITY_WEBGL && !UNITY_EDITOR)
+    FirebaseDatabase.GetJSON(FirebaseUtility.databaseURL+"/USERS", this.gameObject.name, "ParseUserData", "Failure");
+    yield break;
+#else
         isCurrentlyReading = true;
         var task = dbr.Child("USERS").OrderByValue().GetValueAsync();
         yield return new WaitUntil(predicate: () => task.IsCompleted);
@@ -46,15 +53,36 @@ public class FirebaseLeaderboard : MonoBehaviour
             //List<>
             foreach(var user in snapshot.Children)
             {
-                string userInfo = $"{user.Key}-{user.Value}";
-                //Debug.Log(userInfo);
-                GameObject GO = Instantiate(scorePrefab, content);
-                GO.GetComponentInChildren<TextMeshProUGUI>().text = userInfo;
-                GO.transform.SetAsFirstSibling();
+                GenerateScoreInfo(user.Key, user.Value.ToString());
             }
             //for()
         }
-
-    }
     #endif
+    }
+#if (UNITY_WEBGL && !UNITY_EDITOR)
+public void ParseUserData(string json)
+{
+    var snapshot = JSON.Parse(json);
+    foreach (var User in snapshot.Children)
+    {
+        foreach (var UserName in User.Keys)
+        {
+            GenerateScoreInfo(UserName, User.GetValueOrDefault(UserName, null)?.ToString()??"0");
+        }
+    }
+}
+#endif
+    public void GenerateScoreInfo(string username, string value)
+    {
+        string userInfo = $"{username}-{value}";
+        //Debug.Log(userInfo);
+        GameObject GO = Instantiate(scorePrefab, content);
+        GO.GetComponentInChildren<TextMeshProUGUI>().text = userInfo;
+        GO.transform.SetAsFirstSibling();
+    }
+    
+    public void Failure(string error)
+    {
+        FirebaseUtility.Failure(error);
+    }
 }
---
+++ Assets/Scripts/FirebaseScripts/FirebaseUtility.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FirebaseUtility
+{
+    public const string databaseURL = "https://thunkable-3c1e0.firebaseio.com/";
+
+    public static void Success(string value)
+    {
+
+    }
+
+    public static void Failure(string error)
+    {
+
+    }
+}
